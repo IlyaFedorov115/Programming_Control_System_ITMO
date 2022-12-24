ac808cc46327a7de68d0f03be1bd14ebd2899002
IlyaFedorov115 <42892348+IlyaFedorov115@users.noreply.github.com>
2022-12-24 21:55:47 +0300

    add_matlab_part

---
+++ Programming_ITMO/Lecture_1.m
@@ -0,0 +1,27 @@
+num = [1 4 4 2];
+den = [1 4 4 1];
+
+Gp = tf(num, den)
+
+% observable form
+GpssObs = canon(Gp,'companion');
+GpssObsA = GpssObs.A;
+GpssObsB = GpssObs.B;
+GpssObsC = GpssObs.C;
+GpssObsD = GpssObs.D;
+
+% controll form (not sure)
+GpssConA = GpssObsA.';
+GpssConB = GpssObsC.';
+GpssConC = GpssObsB.';
+GpssConD = GpssObsD;
+
+% dicrete controll form
+GpssConA1 = expm(GpssConA*dt1);
+GpssConA2 = expm(GpssConA*dt2);
+GpssConA3 = expm(GpssConA*dt3);
+
+GpssConB1 = inv(GpssConA) * (GpssConA1 - eye(size(GpssConA))) * GpssConB;
+GpssConB2 = inv(GpssConA) * (GpssConA2 - eye(size(GpssConA))) * GpssConB;
+GpssConB3 = inv(GpssConA) * (GpssConA3 - eye(size(GpssConA))) * GpssConB;
+
